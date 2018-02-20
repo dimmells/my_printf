a54dee42401c279fa7029144b00e50e4bd0b0e6b
Dmytro MELNYK <dmelnyk@e1r6p14.unit.ua>
2018-02-20 13:05:09 +0200

    --free

--- get_bin_size.c
+++ get_bin_size.c
@@ -6,7 +6,7 @@
 /*   By: dmelnyk <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/18 15:25:27 by dmelnyk           #+#    #+#             */
-/*   Updated: 2018/02/18 15:29:26 by dmelnyk          ###   ########.fr       */
+/*   Updated: 2018/02/20 13:04:31 by dmelnyk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@ int			get_bin_size(unsigned int wc)
 
 	itoa = itoa_base(wc, 2);
 	size = ft_strlen(itoa);
-	ft_strdel(&itoa);
+//	ft_strdel(&itoa);
 	return (size);
 }
