15f25cfe8a8a621d45af725820a648ef5404abce
Dmytro MELNYK <dmelnyk@e1r6p14.unit.ua>
2018-02-21 11:20:00 +0200

    0 test6

--- add_sign.c
+++ add_sign.c
@@ -6,7 +6,7 @@
 /*   By: dmelnyk <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/15 12:45:32 by dmelnyk           #+#    #+#             */
-/*   Updated: 2018/02/20 12:47:43 by dmelnyk          ###   ########.fr       */
+/*   Updated: 2018/02/21 11:19:54 by dmelnyk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void		add_sign(t_specifier ts, char **print)
 {
 	if (ts.plus == 1)
-		*print = strjoin_n_del("+", *print, 0);
+		*print = strjoin_n_del("+", *print, 2);
 	if (ts.plus == -1)
-		*print = strjoin_n_del("-", *print, 0);
+		*print = strjoin_n_del("-", *print, 2);
 }
